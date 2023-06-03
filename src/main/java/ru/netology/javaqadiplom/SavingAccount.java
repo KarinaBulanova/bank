aecfc11621cb0aa8ee431aa76be9b96675c371da
Maria <smn0101@yandex.ru>
2023-06-03 21:18:08 +0300

    Saving fixed

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -1,6 +1,5 @@
 package ru.netology.javaqadiplom;
 
-import java.time.LocalDate;
 
 /**
  * Сберегательный счёт
@@ -64,12 +63,13 @@ public class SavingAccount extends Account {
             throw new IllegalArgumentException(
                     "Начальный баланс не может быть больше максимального, а у вас: " + initialBalance
             );
-                    }
-                    this.balance = initialBalance;
-                    this.minBalance = minBalance;
-                    this.maxBalance = maxBalance;
-                    this.rate = rate;
-        
+        }
+        this.balance = initialBalance;
+        this.minBalance = minBalance;
+        this.maxBalance = maxBalance;
+        this.rate = rate;
+    }
+
     /**
      * Операция оплаты с карты на указанную сумму.
      * В результате успешного вызова этого метода, баланс должен уменьшиться
@@ -137,69 +137,12 @@ public class SavingAccount extends Account {
 
         return balance / 100 * rate;
     }
-                /**
-                 * Операция оплаты с карты на указанную сумму.
-                 * В результате успешного вызова этого метода, баланс должен уменьшиться
-                 * на сумму покупки. Если же операция может привести к некорректному
-                 * состоянию счёта (например, баланс может уйти в минус), то операция должна
-                 * завершиться вернув false и ничего не поменяв на счёте.
-                 * @param amount - сумма покупки
-                 * @return true если операция прошла успешно, false иначе.
-                 */
-                @Override
-                public boolean pay(int amount) {
-                    if (amount <= 0) {
-                        return false;
-                    }
-                    balance = balance - amount;
-                    if (balance > minBalance) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
 
-                /**
-                 * Операция пополнения карты на указанную сумму.
-                 * В результате успешного вызова этого метода, баланс должен увеличиться
-                 * на сумму покупки. Если же операция может привести к некорректному
-                 * состоянию счёта, то операция должна
-                 * завершиться вернув false и ничего не поменяв на счёте.
-                 * @param amount - сумма пополнения
-                 * @return true если операция прошла успешно, false иначе.
-                 * @param amount
-                 * @return
-                 */
-                @Override
-                public boolean add ( int amount){
-                    if (amount <= 0) {
-                        return false;
-                    }
-                    if (balance + amount < maxBalance) {
-                        balance = balance + amount;
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
+    public int getMinBalance() {
+        return minBalance;
+    }
 
-                /**
-                 * Операция расчёта процентов на остаток счёта при условии, что
-                 * счёт не будет меняться год. Сумма процентов приводится к целому
-                 * числу через отбрасывание дробной части (так и работает целочисленное деление).
-                 * Пример: если на счёте 200 рублей, то при ставке 15% ответ должен быть 30.
-                 * @return
-                 */
-                @Override
-                public int yearChange () {
-                return balance / 100 * rate;
-            }
-
-            public int getMinBalance () {
-                return minBalance;
-            }
-
-            public int getMaxBalance () {
-                return maxBalance;
-            }
-        }
+    public int getMaxBalance() {
+        return maxBalance;
+    }
+}
